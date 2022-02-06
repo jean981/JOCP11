0e1a56fd05a28b8873a8ea93d6b27d39da804dc0
Jean de Sá <jean.sa@celfinet.com>
2022-02-06 23:38:32 +0000

    defined steps of variable scope

---
+++ myVariables/src/main/java/com/certification/variables/variableAppModel.java
@@ -0,0 +1,14 @@
+package com.certification.variables;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class variableAppModel {
+
+    private Integer id;
+    private String nome;
+    private String doc;
+
+}
